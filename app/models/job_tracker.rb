1f3f671896d8694f92cd4b8da45cf20baef578ea
github username <github email address>
2020-01-18 12:08:31 -0700

    models set up and migrations completed

---
+++ app/models/job_tracker.rb
@@ -0,0 +1,5 @@
+class JobTracker < ApplicationRecord
+  belongs_to :user
+  belongs_to :job
+  has_many :jobNotes
+end
---
+++ db/migrate/20200118190632_create_job_notes.rb
@@ -0,0 +1,13 @@
+class CreateJobNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :job_notes do |t|
+      t.references :job_tracker, null: false, foreign_key: true
+      t.text :notes
+      t.text :interview_questions
+      t.text :prep_questions
+      t.text :improvement
+
+      t.timestamps
+    end
+  end
+end
